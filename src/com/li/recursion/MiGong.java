40566d792e76002821f3936878be1965919d3f7b
LiQiuliang <13225542821@163.com>
2021-01-28 23:15:51 +0800

    2021 1  28 23:14

--- src/com/li/recursion/MiGong.java
+++ src/com/li/recursion/MiGong.java
@@ -30,6 +30,10 @@ public class MiGong {
         }
         map[3][1]=1;
         map[3][2]=1;
+       /*  演示死路回溯 放开这俩
+       map[2][3]=1;
+        map[1][3]=1;
+        */
        //输出地图的情况
         System.out.println("---地图的情况---");
         for (int i=0;i<8;i++){
@@ -39,5 +43,69 @@ public class MiGong {
             System.out.println();
         }
 
+        //让小球找路 从 map[1][1] 开始走
+
+        setWay(map, 1, 1);
+        //打印走过后的地图
+     //   System.out.printf("走过的步数为：%d",res);
+        System.out.println("---走过之后地图的情况---");
+        for (int i=0;i<8;i++){
+            for (int j=0;j<7;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    //使用递归回溯来给小球找路
+    // 说明
+    // 1. map 表示地图
+    // 2. i,j 表示从地图的哪个位置开始出发 (1,1)
+    // 3. 如果小球能到 map[6][5] 位置，则说明通路找到.
+    // 4. 约定： 当 map[i][j]
+            // 为 0 表示该点没有走过
+            // 当为 1 表示墙 ；
+            // 2 表示通路可以走 ；
+            // 3 表示该点已经 走过，但是走不通
+    //5. 在走迷宫时，需要确定一个策略(方法) 下->右->上->左 , 如果该点走不通，再回溯
+    /**
+     *
+     * @param map  表示地图
+     * @param i
+     * @param j   int[i][j] 表示从哪里开始走
+     * @return
+     */
+    public static boolean setWay(int[][] map,int i,int j ){
+       // res++;
+        if (map[6][5]==2){//说明 当前小球已经走到了终点
+            return true;
+        }else {//还没有找到终点 接下来按策略走
+            if (map[i][j] == 0) {//表示当前小球还没走到这个格子
+                //假设这个格子能走的通 给这个格子置为2，然后去找下一条路 如果找不到下调路，
+                // 则说明这个格子怒能走通 ，再把这个格子从2置为3，表示当前走的这条路为死路，然后 回溯
+               //原理 会验证下一步走的格子是不是死路 是死路则返回false 则会验证下一个if语句
+                map[i][j] = 2;
+                if (setWay(map, i + 1, j)) {//向下走
+                    return true;
+                } else if (setWay(map, i, j + 1)) { //向右走
+                    return true;
+                } else if (setWay(map, i - 1, j)) { //向上
+                    return true;
+                } else if (setWay(map, i, j - 1)) { // 向左走
+                    return true;
+                }else {//表示 上下左右都走不通 把当前路置为3 表示此路不通
+                    map[i][j]=3;
+                    return false;
+                }
+
+            }else {//map[i][j]!=0 的情况， 即为 1墙  2走过  3死路
+                return false;
+
+            }
+
+
+
+        }
     }
 }
